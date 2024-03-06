81d6dcf6f0e37f29758073883d547232fdcf2732
mrkiedes <akiedes@vtsd.com>
2024-03-06 12:21:46 -0500

    3.1.3 Interation 1 Complete

--- 3.1.3/step3.py
+++ 3.1.3/step3.py
@@ -1,1 +1,19 @@
-print("My stuff")
+sandwich_type = ""
+subtotal = 0.0
+
+# Explaining Menu Options
+print("What type of sandwich would you like?")
+print("chicken $5.25, beef $6.25, tofu $5.75")
+sandwich_type = input("Sandwich Choice: ")
+
+# Calculating Sandwich Price
+if sandwich_type == "chicken":
+    print("You chose chicken, which will be $5.25")
+    subtotal += 5.25
+elif sandwich_type == "tofu":
+    print("You chose tofu, which will be $5.75")
+    subtotal += 5.75
+else:
+    print("You chose beef, which will be $6.25")
+    subtotal += 6.25
+
