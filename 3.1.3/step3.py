c9769f5217e1818d65ac1d308c7035ba672f5d3f
mrkiedes <akiedes@vtsd.com>
2024-03-12 12:16:55 -0400

    3.2.1 Initial Upload

--- 3.1.3/step3.py
+++ 3.1.3/step3.py
@@ -17,3 +17,51 @@ else:
     print("You chose beef, which will be $6.25")
     subtotal += 6.25
 
+# Beverage Choice
+bev_choice = input("Do you want a beverage? yes or no")
+if bev_choice == "yes":
+    bev_size = input("What size beverage would you like? Small, Medium, or Large")
+    if bev_size == "small":
+        print("You chose small, so $1.00")
+        subtotal += 1
+    if bev_size == "medium":
+        print("You chose medium, so $1.75")
+        subtotal += 1.75
+    if bev_size == "large":
+        print("You chose large, so $2.25")
+        subtotal += 2.25
+
+# Fries
+fry_choice = input("Do you want french fries? Yes or No")
+if fry_choice == "yes":
+    fry_size = input("What size fries would you like? Small, Medium, or Large")
+    if fry_size == "small":
+        supersize = input("Do you want to supersize that for $2? Yes or no")
+        if supersize == "yes":
+            fry_size = "large"
+            subtotal += 2
+        else:
+            print("You chose small fries for $1")
+            subtotal += 1
+    elif fry_size == "medium":
+        print("You chose medium for $1.50")
+        subtotal += 1.50
+    else:
+        print("You chose large fries for $2")
+        subtotal += 2
+
+# Kethcup
+ketchup = float(input("HJow many ketup packets would you like? $0.25 each"))
+if ketchup >= 0:
+    subtotal += ketchup * .25
+
+if bev_choice == "yes" and fry_choice == "yes":
+    subtotal -= 1
+
+print(sandwich_type)
+if bev_choice == "yes":
+    print(bev_size)
+if fry_choice == "yes":
+    print(fry_size)
+
+print(subtotal)
---
+++ 3.2.1/ListIntroduction.py
@@ -0,0 +1,9 @@
+# Lists are Key -> Value Pairs
+# We use their index to reference their values
+
+string_list = ["W", "or", "l", "d!"]
+int_list = [5, 15, -67, 191, 88, -23]
+float_list = [2.2, -101.9, 60.0]
+boolean_list = [False, False, True, False, True]
+mixed_list = ["Hello", 2, "the", string_list]
+empty = []
