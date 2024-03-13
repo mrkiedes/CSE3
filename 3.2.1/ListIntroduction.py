2cbf82bd0b2ea22f48bd1ee31dfcdc4cf5885602
mrkiedes <akiedes@vtsd.com>
2024-03-13 11:43:41 -0400

    3.2.1 Second Upload

--- 3.2.1/ListIntroduction.py
+++ 3.2.1/ListIntroduction.py
@@ -7,3 +7,11 @@ float_list = [2.2, -101.9, 60.0]
 boolean_list = [False, False, True, False, True]
 mixed_list = ["Hello", 2, "the", string_list]
 empty = []
+
+print(int_list[5])
+int_list = 23
+print(int_list)
+
+if "hello" in mixed_list:
+    print("It was there!")
+    print("Condition")
