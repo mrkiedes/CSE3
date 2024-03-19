a3cc755af173296ce613d69218e932c6d6e272c9
mrkiedes <akiedes@vtsd.com>
2024-03-19 12:36:18 -0400

    3.2.2 Update

--- 3.2.2/introduction.py
+++ 3.2.2/introduction.py
@@ -1,5 +1,5 @@
 import random
-
+'''
 ########################## Basic user-generated function Start
 def random_number():
     n1 = random.randint(30, 50)
@@ -13,14 +13,25 @@ while x != 100:
     random_number()
     x += 1
 
-import random
-
-########################## Basic user-generated function End
-
-
+###################
+####### Basic user-generated function End
 
 ########################## Basic Parameters Start
+'''
+def larger_val(num1, num2):
+    if num1 > num2:
 
+        return num1
+    elif num1 == num2:
+        print("They are the same")
+        return num2
+    else:
+        return num2
+
+
+biggest_number = larger_val(14, 22)
+
+print(biggest_number)
 
 
 
---
+++ 3.2.2/main.py
@@ -0,0 +1,6 @@
+#Activity 3.2.2 Step 7
+from Post import Post
+
+all_posts_archive = []
+
+# your code here
